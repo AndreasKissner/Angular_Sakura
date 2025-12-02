e04633fbd692bb8fe204f150111d40a614bf4ac0
Andreas Kissner <andikiss500@gmail.com>
2025-12-02 19:00:42 +0100

    Added Base.href and

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { MainContent } from './main-content/main-content';
 import { ImprintComp } from './imprint-comp/imprint-comp';
+import { LandingPageComp } from './main-content/landing-page/landing-page-comp';
 
 export const routes: Routes = [
     { path : '' , component : MainContent },
       { path : 'imprint-comp' , component : ImprintComp },
+       { path : 'landing-page' , component : LandingPageComp },
+
 ];
